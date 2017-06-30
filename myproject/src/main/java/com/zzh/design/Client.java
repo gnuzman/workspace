cab2be52f041744e1087c7bfac641943fb2503ce
gnuzman <94644378@qq.com>
2017-06-30 11:32:05 +0800

    x

---
+++ myproject/src/main/java/com/zzh/design/Client.java
@@ -0,0 +1,16 @@
+package com.zzh.design;
+
+
+/**
+ * Created by house on 2017/6/30.
+ */
+public class Client {
+    public static void main(String args[]) {
+        System.out.println("Design Pattern");
+    }
+
+    public static void singletonTest() {
+        SingletonZ x = new SingletonZ();
+
+    }
+}
---
+++ myproject/src/main/java/com/zzh/design/SingletonZ.java
@@ -0,0 +1,12 @@
+package com.zzh.design;
+
+/**
+ * Created by house on 2017/6/30.
+ */
+public class SingletonZ {
+    private SingletonZ() {
+
+    }
+
+
+}
