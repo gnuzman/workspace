9123d93cbeef86d32410e7f31299996731a54db3
gnuzman <94644378@qq.com>
2017-06-30 16:52:50 +0800

    singleton

--- myproject/src/main/java/com/zzh/design/SingletonZ.java
+++ myproject/src/main/java/com/zzh/design/SingletonZ.java
@@ -4,9 +4,15 @@ package com.zzh.design;
  * Created by house on 2017/6/30.
  */
 public class SingletonZ {
+    private static SingletonZ instance = null;
     private SingletonZ() {
-
     }
 
+    public static SingletonZ getInstance() {
+        if (null == instance) {
+            instance = new SingletonZ();
+        }
 
+        return instance;
+    }
 }
