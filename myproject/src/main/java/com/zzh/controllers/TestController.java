8138b54248532d49979d6c2e89728e2563b89130
gnuzman <94644378@qq.com>
2017-08-03 14:07:10 +0800

    resttemplate

--- myproject/src/main/java/com/zzh/controllers/TestController.java
+++ myproject/src/main/java/com/zzh/controllers/TestController.java
@@ -284,6 +284,52 @@ public class TestController {
         // 填写登录用户名密码
         restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor("username", "passwd"));
 
+//    public static void main(String[] args) {
+//        RestTemplate restTemplate = new RestTemplate();
+
+//get方式***********************************************************************************************************
+
+//	//参数直接放在URL中
+//	String message = restTemplate.getForObject("http://localhost:8080/yongbarservice/appstore/appgoods/restTemplate?name=zhaoshijie&id=80", String.class );
+//
+//
+//	//参数使用MAP传递
+//	Map<String ,Object> urlVariables = new HashMap<String ,Object>();
+//	urlVariables.put("name", "zhaoshijie");
+//	urlVariables.put("id", 80);
+//	String message2 = restTemplate.getForObject("http://localhost:8080/yongbarservice/appstore/appgoods/restTemplate", String.class, urlVariables);
+
+
+
+//delete方式***********************************************************************************************************
+
+//delete方法（注意：delete方法没有返回值，说明，id=0这个参数在服务器端可以不定义该参数，直接使用request获取）
+//	restTemplate.delete("http://localhost:8080/yongbarservice/appstore/appgoods/deleteranking?id=0");
+
+
+
+
+//post方式***********************************************************************************************************
+//使用MAP传递参数
+//	Map<String ,Object> urlVariables = new HashMap<String ,Object>();
+//	urlVariables.put("name", "zhaoshijie");
+//	urlVariables.put("id", 80);
+//	String message3 = restTemplate.postForObject("http://localhost:8080/yongbarservice/appstore/appgoods/restTemplate",null, String.class, urlVariables);
+
+//直接使用URL传递参数
+//	String message = restTemplate.postForObject("http://localhost:8080/yongbarservice/appstore/appgoods/restTemplate?name=zhaoshijie&id=80",null, String.class );
+
+
+
+//put方式***********************************************************************************************************
+//注意：delete方法没有返回值，说明，id=0这个参数在服务器端可以不定义该参数，直接使用request获取
+//        restTemplate.put("http://localhost:8080/yongbarservice/appstore/appgoods/restTemplate?name=zhaoshijie&id=80" ,null);
+
+
+
+//	System.out.println(message);
+//	System.out.println(message2);
+//	System.out.println(message3);
     }
 
 }
