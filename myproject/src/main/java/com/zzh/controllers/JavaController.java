b57dc2cdd157004e1361d876cb5f20e21290af08
gnuzman <94644378@qq.com>
2017-08-08 07:57:00 +0800

    map find

--- myproject/src/main/java/com/zzh/controllers/JavaController.java
+++ myproject/src/main/java/com/zzh/controllers/JavaController.java
@@ -5,12 +5,29 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @CrossOrigin
 @Api(tags = "java")
 public class JavaController {
- @GetMapping("java/currenttime")
+    @GetMapping("java/currenttime")
     public String getCurrentTime() {
-     return "" + System.currentTimeMillis();
- }
+        return "" + System.currentTimeMillis();
+    }
+
+    @GetMapping("java/test-map")
+    public String testMap() {
+        Map<String, Object> test = new HashMap<>();
+        test.put("a", 1);
+        test.put("b", 2);
+        test.put("c", 3);
+
+        if (null == test.get("d")) {
+            return "null";
+        } else {
+            return test.get("d").toString();
+        }
+    }
 }
